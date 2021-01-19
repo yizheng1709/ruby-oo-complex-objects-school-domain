240872d855f8838d0a03153627194c8468c38261
Eva <yi.zheng1709@gmail.com>
2021-01-18 20:58:07 -0500

    commit tree 4af2eba6d79997cb1b67ab88faa56e726b59ae68

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+
+class School 
+
+    attr_accessor :roster
+
+    def initialize(school)
+        @roster = school  
+        @roster = {}
+    end 
+
+    def add_student(name, grade)
+        if !@roster.key?(grade)
+            @roster[grade] = [] 
+            @roster[grade] << name
+        else 
+            @roster[grade] << name
+        end  
+    end 
+
+    def grade(grade)
+        @roster[grade]
+    end 
+
+    def sort
+        empty = {}
+        @roster.each do |grade, name|
+            empty[grade] = name.sort  
+        end  
+        empty.sort.to_h 
+    end 
+
+end 
